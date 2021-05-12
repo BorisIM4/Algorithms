f240aebce8af27ddae41260571f6c02f4473002b
JRB <jrb@abv.bg>
2021-05-12 21:07:57 +0300

    Add bubble sort algorithms

---
+++ src/L1_Sorting_Algorithms/L2_Bubble_Sort/Main.java
@@ -0,0 +1,11 @@
+package L1_Sorting_Algorithms.L2_Bubble_Sort;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
