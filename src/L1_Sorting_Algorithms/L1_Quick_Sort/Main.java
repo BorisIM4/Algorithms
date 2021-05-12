d25361aeabb34bce7379ec789d34655ecba14ffd
JRB <jrb@abv.bg>
2021-05-12 23:13:14 +0300

    Bubble sort corection

--- src/L1_Sorting_Algorithms/L1_Quick_Sort/Main.java
+++ src/L1_Sorting_Algorithms/L1_Quick_Sort/Main.java
@@ -1,30 +1,10 @@
 package L1_Sorting_Algorithms.L1_Quick_Sort;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //90, 80, 70, 60, 50, 40, 30, 20, 10
-        //20, 30, 10, 70, 60, 40, 50, 80, 90
-        int[] array = new int[] {90, 80, 70, 60, 50, 40, 30, 20, 10};
-
-        for (int i = 0; i < array.length; i++) {
-
-            for (int j = 0; j < array.length - 1; j++) {
-                int firstElement = array[j];
-                int secondElement = array[j + 1];
-
-                if (firstElement > secondElement) {
-                    int controlElement = secondElement;
-                    array[j] = controlElement;
-                    array[j + 1] = firstElement;
-                }
-            }
-        }
-
-        Arrays.stream(array).forEach(e -> System.out.print(e + " "));
     }
 }
