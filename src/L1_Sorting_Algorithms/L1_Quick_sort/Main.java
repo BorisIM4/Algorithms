18390e10ab76d7b650197810c7a57da3b2d4d16e
JRB <jrb@abv.bg>
2021-05-12 20:23:44 +0300

    Algorithms First commit

---
+++ src/L1_Sorting_Algorithms/L1_Quick_sort/Main.java
@@ -0,0 +1,11 @@
+package L1_Sorting_Algorithms.L1_Quick_sort;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
